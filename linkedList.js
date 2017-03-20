089b5bc6960fe89fac9f932bf8af51cc6c4f6b85
Dakota <bazookaharwell@gmail.com>
2017-03-20 12:21:57 -1000

    Tests 10-15

--- linkedList.js
+++ linkedList.js
@@ -16,9 +16,46 @@ function linkedListGenerator(){
     return _tail;
   }
 
+  function add(value) {
+
+    if (_head === null) {
+      _head = {
+        value: value,
+        next: null
+      };
+      _tail = _head;
+    } else {
+      _tail.next = {
+        value: value,
+        next: null
+      };
+      _tail = _tail.next;
+    }
+    return _tail;
+  }
+
+  function remove(number) {
+
+  }
+
+  function get(number) {
+
+  }
+
+  function insert(value, number) {
+
+  }
+
   return {
     getHead,
-    getTail
+    getTail,
+    add,
+    remove,
+    get,
+    insert
   };
 
 }
+
+var list = linkedListGenerator();
+console.log(list.add("nice"));
